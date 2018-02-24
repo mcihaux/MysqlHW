026e7b266658a61a180946606410e17519fe7b80
Brandon Michaux <michaux@Brandons-MacBook.local>
2018-02-24 00:39:34 -0500

    updates

--- mysqlhw.js
+++ mysqlhw.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host : "localhost",
     user : "root",
     password : "",
-    database : "Just_Products"
+    database : "bamazon"
 });
 
 // Connecting to the Just_Products Database
